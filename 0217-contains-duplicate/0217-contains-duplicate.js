14ad4c0eec0f5979b2bab7c9c137c64ebe8f455c
Harikrishnan <hkharikrishnan23@gmail.com>
2024-06-14 14:30:53 +0530

    Time: 156 ms (6.19%), Space: 59.1 MB (95.49%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.js
+++ 0217-contains-duplicate/0217-contains-duplicate.js
@@ -3,7 +3,7 @@
  * @return {boolean}
  */
 var containsDuplicate = function (nums) {
-   let sort= nums.sort((a,b)=>a-b)
+   let sort= nums.sort()
     for(let i=0;i<sort.length-1; i++){
         if(nums[i]==nums[i+1]){
             return true;
