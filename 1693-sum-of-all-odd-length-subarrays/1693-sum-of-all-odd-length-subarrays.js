309e66371cd72815fad58a7aebb20ef9e2ff04cc
Harikrishnan <hkharikrishnan23@gmail.com>
2024-06-29 15:49:30 +0530

    Time: 53 ms (73.37%), Space: 48.9 MB (63.59%) - LeetHub

---
+++ 1693-sum-of-all-odd-length-subarrays/1693-sum-of-all-odd-length-subarrays.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} arr
+ * @return {number}
+ */
+var sumOddLengthSubarrays = function (arr) {
+    let result = 0
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = i; j < arr.length; j = j + 2) {
+            for (let k = i; k <= j; k++) {
+                result = result + arr[k]
+            }
+        }
+    }
+    return result
+};
