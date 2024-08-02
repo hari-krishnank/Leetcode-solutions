34a287cf607e556cc40f53781dbfe241739333e5
Harikrishnan <hkharikrishnan23@gmail.com>
2024-08-02 14:23:08 +0530

    Time: 71 ms (65.54%), Space: 57.7 MB (5.66%) - LeetHub

--- 2502-sort-the-people/2502-sort-the-people.js
+++ 2502-sort-the-people/2502-sort-the-people.js
@@ -4,14 +4,17 @@
  * @return {string[]}
  */
 var sortPeople = function (names, heights) {
-    let people = []
-
-    for (let i = 0; i < names.length; i++) {
-        people.push({ name: names[i], height: heights[i] })
+    let arr = []
+    for (i = 0; i < names.length; i++) {
+        arr.push([names[i], heights[i]])
     }
+    let sort = arr.sort((a, b) => b[1] - a[1])
 
-    people.sort((a, b) => b.height - a.height)
-
-    const sortedNames = people.map(person => person.name);
-    return sortedNames
-}
+    let str = []
+    for (i = 0; i < sort.length; i++) {
+        str.push(sort[i][0])
+    }
+    
+    console.log(str)
+    return str
+};
