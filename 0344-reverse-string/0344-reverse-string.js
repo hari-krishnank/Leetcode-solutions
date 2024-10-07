71f718f28841528131cbcc8277e065b8180bb7c7
Harikrishnan <hkharikrishnan23@gmail.com>
2024-10-07 14:46:35 +0530

    Time: 88 ms (13.39%), Space: 57.7 MB (15.98%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.js
+++ 0344-reverse-string/0344-reverse-string.js
@@ -3,20 +3,21 @@
  * @return {void} Do not return anything, modify s in-place instead.
  */
 var reverseString = function (s) {
-    let a = s.join('')
-    console.log(a)
-    let stack = []
+    // let a = s.join('')
+    // console.log(a)
+    // let stack = []
 
 
-    for (let i = 0; i < a.length; i++) {
-        stack.push(a[i])
-    }
-    console.log('stack', stack)
-    let i = 0
-    while (stack.length) {
-        s[i] = stack.pop()
-        i++
-    }
-    console.log('res',s)
-    return s
+    // for (let i = 0; i < a.length; i++) {
+    //     stack.push(a[i])
+    // }
+    // console.log('stack', stack)
+    // let i = 0
+    // while (stack.length) {
+    //     s[i] = stack.pop()
+    //     i++
+    // }
+    // console.log('res',s)
+    // return s
+    s.reverse()
 };
